c88761e0d6bde6011adff090f6e925a05ef9bb5e
Jose Toro <ing.josetoro@hotmail.com>
2023-09-19 08:41:30 -0500

    Create Global Styles and Utilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import "./index.css";
+
 function App() {
-  return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
-  );
+	return (
+		<>
+			{/* Encabezado */}
+			<h1>HBO</h1>
+
+			{/* Párrafo */}
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+				recusandae assumenda minus ad quis expedita quos ex illo iste veniam,
+				pariatur est doloribus nulla repellendus quod amet iusto voluptatum
+				enim!
+			</p>
+
+			{/* Enlace */}
+			<a href="#">Enlace random</a>
+
+			{/* Enlace con variante de color */}
+			<a href="#" className="link--variant">
+				Enlace random con una variante de color
+			</a>
+
+			{/* Botón */}
+			<a href="#" className="btn">
+				BOTON NORMAL
+			</a>
+
+			{/* Botón con variante de color */}
+			<a href="#" className="btn btn--gradient">
+				BOTONCHITO
+			</a>
+
+			{/* Botón con imágen */}
+			<a href="#" className="btn btn--brand">
+				<img src="/public/images/Branding-HBO.png" draggable="false" />
+			</a>
+		</>
+	);
 }
 
 export default App;
